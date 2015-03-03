9a65bba412ea0297af28255bf84beb1e72790ea0
erickzanardo <erickzanardoo@gmail.com>
2015-03-02 22:55:53 -0300

    Templates

--- processors/processor_factory.js
+++ processors/processor_factory.js
@@ -1,6 +1,7 @@
 var rk = require('rekuire');
 var StaticProcessor = rk('processors/static_processor.js');
 var EntitiesProcessor = rk('processors/entities_processor.js');
+var TemplatesProcessor = rk('processors/templates_processor.js');
 
 function ProcessorFactory() {
 }
@@ -8,6 +9,8 @@ function ProcessorFactory() {
 ProcessorFactory.prototype.getProcessor = function(entry) {
   if (entry.parent() == '_entities')
     return new EntitiesProcessor(entry);
+  else if (entry.parent() == '_templates')
+    return new TemplatesProcessor(entry);
   else
     return new StaticProcessor(entry);
 };
